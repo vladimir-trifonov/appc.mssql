632f13b08691860dde4b11e74b2a5fee047e2801
Dawson Toth <dtoth@appcelerator.com>
2014-12-30 15:35:04 -0500

    https://jira.appcelerator.org/browse/API-263

---
+++ conf/example.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	connectors: {
+		'appc.mssql': {
+			user: '', // Your user name.
+			password: '', // Your password.
+			server: '', // Your Azure server.
+			port: 1433, // The port your server is using.
+			database: '', // Your database.
+
+			// If running on Azure, you'll want the following, too:
+			options: {
+				encrypt: true
+			}
+			// Also with Azure, make sure you whitelist your current IP address (from your Azure dashboard)!
+		}
+	}
+};

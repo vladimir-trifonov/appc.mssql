4acc322119b91489f0b71c13a5943f3e74037ae6
Aleksandrina Karakehayova <akarakehayova@axway.com>
2017-03-17 14:13:58 +0200

    Updated tests for create and save methods

--- test/unit/methods/save.js
+++ test/unit/methods/save.js
@@ -77,7 +77,7 @@ test('### Save without result ###', function (t) {
   })
 })
 
-test('### Save without error ###', function (t) {
+test('### Save successfully ###', function (t) {
     // Data
   var sandbox = sinon.sandbox.create()
   const Model = ARROW.getModel('Posts')
@@ -100,6 +100,64 @@ test('### Save without error ###', function (t) {
 
   const getPrimaryKeyColumnStub = sandbox.stub(CONNECTOR, 'getPrimaryKeyColumn', function (Model) {
     return 'id'
+  })
+  var instance = Model.instance({ title: test, content: 'test content' }, false)
+
+  const sqlStub = sandbox.stub(
+        sql,
+        'Request',
+        (connection) => {
+          return {
+            input: function (param, varChar, id) { },
+            query: function (query, saveQueryCallback) {
+              setImmediate(function () { saveQueryCallback(null, [{ title: 'test' }]) })
+            }
+          }
+        }
+    )
+
+  const addValuesToSQLRequestStub = sandbox.stub(CONNECTOR, 'addValuesToSQLRequest', function (Model, values, request, excludeTimeStamp) { })
+  const transformRow = sandbox.stub(CONNECTOR, 'transformRow', function (Model, row) { return row })
+  function cb (errParameter, instance) { }
+  const cbSpy = sandbox.spy(cb)
+
+    // Execution
+  saveMethod.bind(CONNECTOR, Model, instance, cbSpy)()
+  setImmediate(function () {
+    t.ok(getTableNameStub.calledOnce)
+    t.ok(getPrimaryKeyColumnStub.calledOnce)
+    t.ok(sqlStub.calledOnce)
+    t.ok(addValuesToSQLRequestStub.calledOnce)
+    t.ok(transformRow.calledOnce)
+    t.ok(cbSpy.called)
+    sandbox.restore()
+    t.end()
+  })
+})
+
+test('### Save successfully without primary key ###', function (t) {
+    // Data
+  var sandbox = sinon.sandbox.create()
+  const Model = ARROW.getModel('Posts')
+  Model.instance = function (values, skip) {
+    return {
+      title: 'test',
+      content: 'test content',
+      toPayload () {
+        return ['test', 'test content']
+      },
+      setPrimaryKey (id) { },
+      getPrimaryKey () { return 'id' }
+    }
+  }
+
+    // Stubs & spies
+  const getTableNameStub = sandbox.stub(CONNECTOR, 'getTableName', function (Model) {
+    return 'Posts'
+  })
+
+  const getPrimaryKeyColumnStub = sandbox.stub(CONNECTOR, 'getPrimaryKeyColumn', function (Model) {
+    return undefined
   })
   var instance = Model.instance({ title: test, content: 'test content' }, false)
 
